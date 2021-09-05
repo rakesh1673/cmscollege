9da120b8940bba44a01c2ad7207b8b6e81777440
Rakesh Munaga <munagarakesh1673@gmail.com>
2021-09-05 22:10:19 +0530

    'navbar-update'

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -38,81 +38,100 @@ function Navbar() {
 
   return (
     <div className={`navbar ${show && "navbar-red"}`}>
-      <nav>
-        <div className="navbar-container">
-          <Link to="/">
-            <img
-              src="https://cmscollege.ac.in/wp-content/themes/cms/assets/images/cms-college-kottayam.png"
-              className="cms-logo"
-              alt="CMS College"
-            />
-          </Link>
-          <div className="menu-icon" onClick={handleClick}>
-            <i className={click ? "fas fa-times" : "fas fa-bars"} />
-          </div>
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <Link
-                to="/aboutus"
-                className="nav-links"
-                onClick={closeMobileMenu}
-              >
-                About us
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/organisation"
-                className="nav-links"
-                onClick={closeMobileMenu}
-              >
-                Organisation
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/iqac" className="nav-links" onClick={closeMobileMenu}>
-                IQAC
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/academics"
-                className="nav-links"
-                onClick={closeMobileMenu}
-              >
-                Academics
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/admissions"
-                className="nav-links"
-                onClick={closeMobileMenu}
-              >
-                Admissions
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/examinations"
-                className="nav-links"
-                onClick={closeMobileMenu}
-              >
-                Examinations
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/campuslife"
-                className="nav-links"
-                onClick={closeMobileMenu}
-              >
-                Campus Life
-              </Link>
-            </li>
-          </ul>
+      <div className="top-nav">
+        <div>
+          <nav className="nav-1">
+            <li className="nav-item">nirf</li>
+            <li className="nav-item">Quotations</li>
+            <li className="nav-item">Tenders</li>
+            <li className="nav-item">Downloads</li>
+            <li className="nav-item">Latest Images</li>
+            <li className="nav-item">Media Reports</li>
+            <li className="nav-item">Students Login</li>
+            <li className="nav-item">Faculty Login</li>
+            <li className="nav-item">Contact us</li>
+          </nav>
         </div>
-      </nav>
+        <nav className="nav-2">
+          <div className="navbar-container">
+            <Link to="/">
+              <img
+                src="https://cmscollege.ac.in/wp-content/themes/cms/assets/images/cms-college-kottayam.png"
+                className="cms-logo"
+                alt="CMS College"
+              />
+            </Link>
+            <div className="menu-icon" onClick={handleClick}>
+              <i className={click ? "fas fa-times" : "fas fa-bars"} />
+            </div>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item">
+                <Link
+                  to="/aboutus"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  About us
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/organisation"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Organisation
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/iqac"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  IQAC
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/academics"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Academics
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/admissions"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Admissions
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/examinations"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Examinations
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/campuslife"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Campus Life
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 }
